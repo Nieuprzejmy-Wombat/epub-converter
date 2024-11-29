e788ff2c4f02758e245d13b58c11e5f04456c9cb
wombat <u58_makcza_waw@technischools.com>
2024-11-29 14:47:37 +0100

    some XML tinkering

--- epub-converter/epub.cpp
+++ epub-converter/epub.cpp
@@ -3,50 +3,56 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <ostream>
 #include <string>
 #include <string_view>
 #include <vector>
 
-class File {
+class FileSystemResource {
+public:
+  virtual void write() = 0;
+};
+
+class File : public FileSystemResource {
 protected:
   std::string_view m_filename;
   File(std::string_view filename) : m_filename{filename} {};
-
-public:
-  virtual void write() = 0;
 };
 
 class XMLTag {
 protected:
   std::string_view m_name;
   std::map<std::string_view, std::string_view> m_attributes;
-  std::vector<XMLTag> m_children;
-  bool m_chidrenless;
+  std::vector<std::unique_ptr<XMLTag>> m_children;
 
 public:
   XMLTag(std::string_view name,
-         std::map<std::string_view, std::string_view> attributes = {})
-      : m_name{name}, m_attributes{attributes}, m_children{{}},
-        m_chidrenless{true} {};
-  XMLTag(std::string_view name,
          std::map<std::string_view, std::string_view> attributes,
-         std::vector<XMLTag> children)
-      : m_name{name}, m_attributes{attributes}, m_children{children},
-        m_chidrenless{false} {};
+         std::vector<std::unique_ptr<XMLTag>> children = {})
+      : m_name{name}, m_attributes{attributes}, m_children{children} {};
+  XMLTag(std::string_view name,
+         std::vector<std::unique_ptr<XMLTag>> children = {})
+      : m_name{name}, m_attributes{{}}, m_children{children} {};
+
   friend std::ostream &operator<<(std::ostream &stream, const XMLTag &tag);
+  friend const std::string toString(const XMLTag &tag);
 };
 
-std::ostream &operator<<(std::ostream &stream, const XMLTag &tag) {
-  stream << "<" << tag.m_name;
+const std::string toString(const XMLTag &tag) {
+  std::string res = "<" + std::string{tag.m_name};
   for (auto [key, value] : tag.m_attributes)
-    stream << " " << key << "=\"" << value << "\"";
-  if (tag.m_chidrenless)
-    return stream << "/>\n";
-  stream << ">";
-  for (auto child : tag.m_children)
-    stream << child;
-  return stream << "</" << tag.m_name << ">\n";
+    res += " " + std::string{key} + "=\"" + std::string{value} + "\"";
+  if (tag.m_children.empty())
+    return res + " />\n";
+  res += ">";
+  for (auto i{tag.m_children.begin()}; i != tag.m_children.end(); i++)
+    res += toString(**i);
+  return res + "</" + std::string{tag.m_name} + ">\n";
+}
+
+std::ostream &operator<<(std::ostream &stream, const XMLTag &tag) {
+  return stream << toString(tag);
 }
 
 class XMLStringTag : public XMLTag {
@@ -56,16 +62,18 @@ public:
   XMLStringTag(std::string_view name,
                std::map<std::string_view, std::string_view> attributes,
                std::string_view contents)
-      : XMLTag(name, attributes), m_contents{contents} {};
-  friend std::ostream &operator<<(std::ostream &stream,
-                                  const XMLStringTag &tag);
+      : XMLTag{name, attributes}, m_contents{contents} {};
+  XMLStringTag(std::string_view name, std::string_view contents)
+      : XMLTag{name}, m_contents{contents} {};
+  friend const std::string toString(const XMLStringTag &tag);
 };
 
-std::ostream &operator<<(std::ostream &stream, const XMLStringTag &tag) {
-  stream << "<" << tag.m_name;
+const std::string toString(const XMLStringTag &tag) {
+  std::string res = "<" + std::string{tag.m_name};
   for (auto [key, value] : tag.m_attributes)
-    stream << " " << key << "=\"" << value << "\"";
-  return stream << ">" << tag.m_contents << "</" << tag.m_name << ">\n";
+    res += " " + std::string{key} + "=\"" + std::string{value} + "\"";
+  return res + ">" + std::string{tag.m_contents} + "</" +
+         std::string{tag.m_name} + ">\n";
 }
 
 class XMLFile : public File {
@@ -80,10 +88,10 @@ public:
     stream << *this;
     stream.close();
   }
-  friend std::ostream &operator<<(std::ostream &stream, const XMLFile &file);
+  friend std::ostream &operator<<(std::ostream &stream, XMLFile &file);
 };
 
-std::ostream &operator<<(std::ostream &stream, const XMLFile &file) {
+std::ostream &operator<<(std::ostream &stream, XMLFile &file) {
   stream << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << "\n";
   stream << file.m_body;
   return stream;
@@ -98,70 +106,73 @@ public:
                 "container",
                 {{"xmlns", "urn:oasis:names:tc:opendocument:xmlns:container"},
                  {"version", "1.0"}},
-                {XMLTag("rootfiles", {},
-                        {XMLTag("rootfile",
-                                {{"full-path", "EPUB/package.opf"},
-                                 {"media-type",
-                                  "application/oebps-package+xml"}})})})){};
+                {std::make_unique<XMLTag>(
+                    "rootfiles",
+                    std::vector<std::unique_ptr<XMLTag>>{
+                        std::make_unique<XMLTag>(
+                            "rootfile",
+                            std::map<std::string_view, std::string_view>{
+                                {"full-path", "EPUB/package.opf"},
+                                {"media-type",
+                                 "application/oebps-package+xml"}})})})) {
+
+        };
 };
 
-class Meta : File {
+class Meta : FileSystemResource {
   ContainerFile m_container;
 
 public:
-  Meta(ContainerFile container) : File("META-INF"), m_container{container} {};
+  Meta(ContainerFile container) : m_container{container} {};
   void write() {
-    system("mkdir META-INF");
+    system("mkdir -p META-INF");
     m_container.write();
   };
 };
 
-class ManifestItem {
-  std::string m_href;
-  std::string m_id;
-  std::string m_type;
+class ManifestItem : XMLTag {
   // properties??
-  friend std::ostream &operator<<(std::ostream &stream, ManifestItem &item);
+  ManifestItem(std::string href, std::string id, std::string type)
+      : XMLTag("item", {{"href", href}, {"id", id}, {"media-type", type}}) {};
 };
 
-std::ostream &operator<<(std::ostream &stream, ManifestItem &item) {
-  return stream << XMLTag("item", {{"href", item.m_href},
-                                   {"id", item.m_id},
-                                   {"media-type", item.m_type}});
-}
-
-class SpineItem {
-  std::string_view m_id;
+class SpineItem : XMLTag {
   // linear??
-  friend std::ostream &operator<<(std::ostream &stream, SpineItem &item);
+  SpineItem(std::string id) : XMLTag{"itemref", {{"idref", id}}} {};
 };
-
-std::ostream &operator<<(std::ostream &stream, SpineItem &item) {
-  return stream << XMLTag("itemref", {{"idref", item.m_id}});
-}
 
 class PackageFile : public XMLFile {
 public:
   PackageFile(std::string_view id, std::string_view title,
               std::string_view creator, std::string_view language,
-              std::vector<XMLTag> manifest, std::vector<XMLTag> spine)
+              std::vector<std::unique_ptr<XMLTag>> manifest,
+              std::vector<std::unique_ptr<XMLTag>> spine)
       : XMLFile( // dcterms:modified ??
             "EPUB/package.opf",
-            XMLTag("package",
-                   {{"xmlns", "http://www.idpf.org/2007/opf"},
-                    {"version", "3.0"},
-                    {"unique-identifier", "uid"}},
-                   {XMLTag("metadata",
-                           {{"xmlns:dc", "http://purl.org/dc/elements/1.1/"}},
-                           {XMLStringTag("dc:identifier", {{"id", "uid"}}, id),
-                            XMLStringTag("dc:title", {}, title),
-                            XMLStringTag("dc:creator", {}, creator),
-                            XMLStringTag("dc:language", {}, language)}),
-                    XMLTag("manifest", {}, manifest),
-                    XMLTag("spine", {}, spine)})) {}
+            XMLTag(
+                "package",
+                {{"xmlns", "http://www.idpf.org/2007/opf"},
+                 {"version", "3.0"},
+                 {"unique-identifier", "uid"}},
+                {std::make_unique<XMLTag>(
+                     "metadata",
+                     std::map<std::string_view, std::string_view>{
+                         {"xmlns:dc", "http://purl.org/dc/elements/1.1/"}},
+                     std::vector<std::unique_ptr<XMLTag>>{
+                         std::make_unique<XMLStringTag>(
+                             "dc:identifier",
+                             std::map<std::string_view, std::string_view>{
+                                 {"id", "uid"}},
+                             id),
+                         std::make_unique<XMLStringTag>("dc:title", title),
+                         std::make_unique<XMLStringTag>("dc:creator", creator),
+                         std::make_unique<XMLStringTag>("dc:language",
+                                                        language)}),
+                 std::make_unique<XMLTag>("manifest", manifest),
+                 std::make_unique<XMLTag>("spine", spine)})) {}
 };
 
-class ContentFile : File {
+class ContentFile : public File {
   std::string_view m_content;
 
 public:
@@ -175,27 +186,26 @@ public:
   };
 };
 
-class Content : File {
+class Content : FileSystemResource {
   PackageFile m_package;
-  ContentFile &m_content;
+  FileSystemResource &m_content;
 
 public:
-  Content(PackageFile package, ContentFile &content)
-      : File("EPUB"), m_package{package}, m_content{content} {};
+  Content(PackageFile package, FileSystemResource &content)
+      : m_package{package}, m_content{content} {};
   void write() {
-    system("mkdir EPUB");
+    system("mkdir -p EPUB");
     m_package.write();
     m_content.write();
   }
 };
 
-class Epub : File {
+class Epub : FileSystemResource {
   Meta m_meta;
   Content m_content;
 
 public:
-  Epub(Meta meta, Content content)
-      : File("epub"), m_meta{meta}, m_content{content} {};
+  Epub(Meta meta, Content content) : m_meta{meta}, m_content{content} {};
   void write() {
     m_meta.write();
     m_content.write();
@@ -204,10 +214,10 @@ public:
 };
 
 int main() {
-  Meta meta{ContainerFile()};
-  ContentFile file = ContentFile("EPUB/plik", "<html></html>");
+  Meta meta{ContainerFile{}};
+  ContentFile file = ContentFile{"EPUB/plik", "<html></html>"};
   Content content =
-      Content(PackageFile("1", "tytuł", "autor", "polski", {}, {}), file);
+      Content(PackageFile("1", "tytuł", "autor", "pl", {}, {}), file);
   Epub epub = Epub(meta, content);
   epub.write();
   return 0;
