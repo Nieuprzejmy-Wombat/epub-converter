c11d80e4f8ff3a44daac83a3b19da0bdff48e8b4
Nieuprzejmy-Wombat <u58_makcza_waw@technischools.com>
2025-01-21 13:43:55 +0100

    added util::now()

---
+++ epub-converter/util.cpp
@@ -0,0 +1,10 @@
+#include <ctime>
+#include <string>
+
+std::string now(){
+  time_t now;
+  time(&now);
+  char buf[sizeof "2011-10-08T07:07:09Z"];
+  strftime(buf, sizeof buf, "%FT%TZ", gmtime(&now));
+  return buf;
+}
